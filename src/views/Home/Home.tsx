4c470596269dc975b6e9cc1dd5bde12ab3fe2d23
Mustafa YILDIZ <mstfyldz.bjk@icloud.com>
2021-01-28 20:45:42 +0300

    bla bladd

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -4,9 +4,8 @@ import { Heading, Text, BaseLayout } from '@pancakeswap-libs/uikit'
 import useI18n from 'hooks/useI18n'
 import Page from 'components/layout/Page'
 import FarmStakingCard from './components/FarmStakingCard'
-import LotteryCard from './components/LotteryCard'
 import CakeStats from './components/CakeStats'
-import TotalValueLockedCard from './components/TotalValueLockedCard'
+
 
 const Hero = styled.div`
   align-items: center;
@@ -58,7 +57,7 @@ const Home: React.FC = () => {
   return (
     <Page>
       <Hero>
-        <Heading as="h1" size="xl" mb="24px" color="secondary">
+        <Heading as="h1" size="xl" mb="24px" color="rgba(255, 0, 0, 1">
           {TranslateString(576, 'Bean Finance')}
         </Heading>
         <Text>{TranslateString(578, 'The #1 Coffee Maker on Binance Smart Chain.')}</Text>
@@ -66,8 +65,7 @@ const Home: React.FC = () => {
       <div>
         <Cards>
           <FarmStakingCard />
-          <TotalValueLockedCard />
-        
+          <CakeStats />
          
         </Cards>
       </div>
